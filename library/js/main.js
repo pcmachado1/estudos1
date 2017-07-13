65d40e9166f81c2af831e20acee8f8ceac57b822
pcmachado <paulo_chm@hotmail.com>
2017-07-12 22:30:00 -0300

    modifications on project

--- library/js/main.js
+++ library/js/main.js
@@ -46,7 +46,8 @@ $(document).ready(function(){
                           console.log('usuario logado com sucesso');
                           location.assign('/estudos1/?page=welcome');
                        }else{
-                          console.log('Nao foi possivel logar');  
+                          console.log('Nao foi possivel logar'); 
+                          $('#errorLoggedOn').html('Usuario ou Senha Incorretos').show().fadeOut(10000);
                        }
                                 }
                 );
@@ -72,6 +73,16 @@ $(document).ready(function(){
             );
     });
     
+    $('#txtEndereco').on("keyup", function() {
+			    var value = $(this).val();
+                            var key = "AIzaSyDvuiU-Vrf_qI7g0bJOsMh-2PWeBg7nufc";
+                            console.log(value);
+                            $.get('https://maps.googleapis.com/maps/api/geocode/json?',{address:value,key:key},function(data){
+                                console.log(data);
+                                console.log(data.results[0].geometry.location);
+                                locationMap(data);
+                            })
+                        });
     
     
     
@@ -99,14 +110,115 @@ function loadUsers(){
         }
     });
 }
-function location(){
+function locationMap(data){
+    //var latLng = {lat:30,lng:30}
     // Create a new map that is immediately displayed on the web page
-    var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 6,
-    center: new google.maps.LatLng(54, 12),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    var mapa = document.getElementById('map');
+    var map = new google.maps.Map(mapa, {
+    zoom: 20,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    disableDefaultUI: false,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+    style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+    },
+    navigationControl: true,
+    navigationControl: true,
+    navigationControlOptions: {
+    position: google.maps.ControlPosition.LEFT,
+    style: google.maps.NavigationControlStyle.ANDROID
+    },
+    scaleControl: true,
+    streetViewControl: false,
+    noClear: true,
     });
+   
+    map.setCenter(data.results[0].geometry.location);
+    // Create the marker
+    var marker = new google.maps.Marker({
+    position: data.results[0].geometry.location
+    });
+    // And add it to a map
+    //formatando endereco
+    var enderecoFull = data.results[0].formatted_address.split('-');
     
-    console.log(map);
+    console.log(enderecoFull);
+    //
+    var enderecoTipo = data.results[0].types[0];
+    
+    switch (enderecoTipo) {
+        
+        case '':
+            console.log('nao fazer nada');
+            break;
+        
+        case 'street_address':
+            console.log('endereco por endereco');
+            break;
+           
+        case 'postal_code':
+            console.log('endereco por cep');
+            break;
+            
+        default:
+            
+            break;
+    }
+    
+    // Create a new InfoWindow object that will be positioned at a specific location
+    var infoWindow = new google.maps.InfoWindow({
+    content:"<nav class=\"navbar navbar-inverse\" >Barra</nav>"+ 
+            "<p>"+enderecoFull[0]+"</p>"+
+            "<p>"+enderecoFull[1]+"</p>"+
+            "<p>"+enderecoFull[2]+"</p>"+
+            "<p>"+enderecoFull[3]+"</p>"+
+            "<p>"+map.getCenter()+"</p>",
+    position: data.results[0].geometry.location
+    });
+    // Add the infoWindow to the map
+     //
+    google.maps.event.addListener(infoWindow, 'domready', function() {
+
+   // Referência ao DIV que recebe o conteúdo da infowindow recorrendo ao jQuery
+   var iwOuter = $('.gm-style-iw');
+   
+   iwOuter.css({'background-color' : '#fff','border-radius':'7px','top':'15px','height':'400px'});
+   
+   /* Uma vez que o div pretendido está numa posição anterior ao div .gm-style-iw.
+    * Recorremos ao jQuery e criamos uma variável iwBackground,
+    * e aproveitamos a referência já existente do .gm-style-iw para obter o div anterior com .prev().
+    */
+   var iwBackground = iwOuter.prev();
+
+   // Remover o div da sombra do fundo
+   iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+   // Remover o div de fundo branco
+   iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+   
+   var iwCloseBtn = iwOuter.next();
+   
+   iwCloseBtn.css({'right':'20px','top':'20px'});
+   
+   iwOuter.parent().parent().css({'left': '130px'});
+   
+   // Desloca a sombra da seta a 76px da margem esquerda 
+iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+// Desloca a seta a 76px da margem esquerda 
+iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+    });
+    //
+    
+    
+    
+    
+    //marker.setMap(map);
+    
+    infoWindow.open(map);
+
+    
+    console.log(infoWindow);
 }
 
